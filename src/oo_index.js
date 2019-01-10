22c3c5caaa4438463a27c707571210ef114e717d
rdaley321 <rdaley321@yahoo.com>
2019-01-10 16:56:58 -0600

    todo working

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,24 @@
+let inputField = document.querySelector('#new-task-description')
+let form = document.querySelector('#create-task-form')
+let tasks = document.querySelector('#tasks')
+let taskList
+
+function render(){
+  tasks.innerHTML = ''
+  taskList.arr.forEach(function(task){
+    let li = document.createElement('li')
+    li.innerText = task.content
+    tasks.append(li)
+  })
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
+  taskList = new TaskList();
 });
+
+form.addEventListener('submit', function(e){
+  e.preventDefault()
+  taskList.createTask(inputField.value)
+  inputField.value = ''
+  render()
+})
--- src/taskList.js
+++ src/taskList.js
@@ -1,2 +1,10 @@
 class TaskList {
+  constructor(){
+    this.arr = []
+  }
+
+  createTask(content){
+    let task = new Task(content)
+    this.arr.push(task)
+  }
 }

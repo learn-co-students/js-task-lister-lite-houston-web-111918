94d18b43a80987bd1320b4f0737cf9df139ebda0
Austin <Austin_MacBook@austins-mbp.attlocal.net>
2019-01-10 21:08:31 -0600

    Done.

--- src/task.js
+++ src/task.js
@@ -1,2 +1,10 @@
 class Task {
+  constructor(content) {
+    this.content = content;
+  }
+
+  render() {
+    return `<li>${this.content} <button data-content="${this.content}">Delete</button></li>`
+  }
+
 }
